a04c32ea431595739cd34b576229ceaa434c53f8
Jiang Menghao <jj704768703@gmail.com>
2020-06-05 20:24:06 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="header-back">
+        <span class="iconfont back-icon">&#xe635;</span>
+      </div>
+      <div class="header-search">
+        <span class="iconfont">&#xe665;</span>
+        <span>输入城市/景点/游玩主题</span>
+        </div>
+      <div class="header-city">
+        <span>城市</span>
+        <span class="iconfont arrow-icon">&#xe639;</span>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    background-color: $bgColor
+    color: #fff
+    .header-back
+      width: .64rem
+      text-align: center
+    .header-search
+      flex: 1
+      padding: 0 .2rem
+      color: #ddd
+      line-height: .62rem
+      background-color #fff
+      border-radius: .1rem
+      margin-top: .12rem
+      margin-left: .2rem
+      height: .62rem
+    .header-city
+      width: 1.24rem
+      text-align: center
+      .arrow-icon
+        margin-left: -.04rem
+</style>
